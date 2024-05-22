66d8445805c0d655226f31f7e4b673b7b6f34035
Antonio Perales Dev <antonioperalesdev@gmail.com>
2024-05-22 12:58:27 -0700

    Contrullendo el (HttpRequest) y el (HttpResponse)

--- src/com/antonioperalesdev/currencyconverter/controller/ControllerApi.java
+++ src/com/antonioperalesdev/currencyconverter/controller/ControllerApi.java
@@ -1,13 +1,32 @@
 package com.antonioperalesdev.currencyconverter.controller;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ControllerApi {
-    private HttpClient cliente;
+    private HttpClient client;
 
     public ControllerApi() {
-        cliente = HttpClient.newHttpClient();
+        client = HttpClient.newHttpClient();
     }
 
+    public String getCurrencyInformation(String moneda){
 
+        try {
+            String address = "https://v6.exchangerate-api.com/v6/b296a8daf9a59a9f6cc260a5/latest/" +
+                    moneda;
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(address))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        }
+        catch (Exception e){
+            throw new RuntimeException("Error al obtener la tassa de cambio", e);
+        }
+    }
 }
