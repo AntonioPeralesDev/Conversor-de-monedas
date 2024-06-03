6a273ced94c0158ffd10841e34509f311cc1c2da
Antonio Perales Dev <antonioperalesdev@gmail.com>
2024-06-02 19:48:15 -0700

    Añadiendo LocalDateTime a currency

--- src/com/antonioperalesdev/currencyconverter/models/Currency.java
+++ src/com/antonioperalesdev/currencyconverter/models/Currency.java
@@ -1,14 +1,22 @@
 package com.antonioperalesdev.currencyconverter.models;
 
+import java.time.LocalDateTime;
+
 public class Currency {
     private String baseCurrency;
     private String targetCurrency;
     private double conversionRate;
+    private LocalDateTime dateTime;
 
     public Currency(CurrencyApi currencyApi){
         this.baseCurrency = currencyApi.base_code();
         this.targetCurrency = currencyApi.target_code();
         this.conversionRate = currencyApi.conversion_rate();
+        this.dateTime = LocalDateTime.now();
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
     }
 
     public String getBaseCurrency() {
